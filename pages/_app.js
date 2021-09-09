5f614869de37d1b89b3a2428be2df968f709794a
Ahsan Nazir <psychoahsan@gmail.com>
2021-09-09 12:45:46 +0530

    intial commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,12 @@
 import 'tailwindcss/tailwind.css'
+import Layout from '../components/layout/Layout'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Component {...pageProps} />
+    </>
+  )
 }
 
 export default MyApp
